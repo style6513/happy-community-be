e74e1e91803019c073eeb90b5a661b0feb88c3d2
Jae Cho <jc960116@gmail.com>
2021-11-15 11:51:25 -0800

    fix authmiddlewware

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -5,42 +5,45 @@ const { UnauthorizedError } = require("../ExpressError");
 function authenticateJWT(req, res, next) {
    try {
       const authHeader = req.headers && req.headers.authorization;
-      if(authHeader) {
+      if (authHeader) {
          const token = authHeader.replace(/^[Bb]earer /, "").trim();
          res.locals.user = jwt.verify(token, SECRET);
       }
       return next();
-   } catch(e) {
+   } catch (e) {
       return next();
    }
 }
 
 function ensureLoggedIn(req, res, next) {
    try {
-      if(!res.locals.user) throw new UnauthorizedError();
-   } catch(e) {
+      if (!res.locals.user) throw new UnauthorizedError();
+      return next();
+   } catch (e) {
       return next(e);
    }
 }
 
 function ensureAdmin(req, res, next) {
    try {
-      if(!res.locals.user || !res.locals.user.isAdmin) {
+      if (!res.locals.user || !res.locals.user.isAdmin) {
          throw new UnauthorizedError();
       }
-   } catch(e) {
+      return next();
+   } catch (e) {
       return next(e);
    }
 }
 
-function ensureCorrectUserOrAdmin(req ,res, next) {
+function ensureCorrectUserOrAdmin(req, res, next) {
    try {
       const user = res.locals.user;
-      if(
-         !user && 
-            (user.isAdmin || user.id === req.params.id)
+      if (
+         !user &&
+         (user.isAdmin || user.id === req.params.id)
       ) throw new UnauthorizedError();
-   } catch(e) {
+      return next();
+   } catch (e) {
       return next(e);
    }
 }
