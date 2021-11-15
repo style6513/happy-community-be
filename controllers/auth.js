3211454882af7530b746bc5b4a380c3dae4c5129
Dae Young Hwang <daeh@Daes-Air.attlocal.net>
2021-11-15 11:16:40 -0800

    pull

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,44 +8,44 @@ const jwt = require("jsonwebtoken");
 
 // REGISTER
 router.post("/register", async (req, res, next) => {
-   const { username, email, password, phone } = req.body;
-   const hashedPw = await bcrypt.hash(password, 12);
-   const newUser = new User({
-      username,
-      email,
-      password: hashedPw,
-      phone
-   });
-   try {
-      const savedUser = await newUser.save();
-      return res.status(201).json(savedUser);
-   } catch (e) {
-      return next(e);
-   }
+    const { username, email, password, phone } = req.body;
+    const hashedPw = await bcrypt.hash(password, 12);
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPw,
+        phone
+    });
+    try {
+        const savedUser = await newUser.save();
+        return res.status(201).json(savedUser);
+    } catch (e) {
+        return next(e);
+    }
 })
 
 // Login 
 router.post("/login", async (req, res, next) => {
-   try {
-      const user = await User.findOne({ username: req.body.username });
-      if (!user) throw new UnauthorizedError();
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) throw new UnauthorizedError();
 
-      const isValid = await bcrypt.compare(password, user.password);
-      if (isValid) {
-         const accessToken = jwt.sign({
-            id: user._id,
-            isAdmin: user._isAdmin || false
-         },
-            SECRET,
-            { expiresIn: "3d" }
-         );
-         const { password, ...others } = user._doc;
-         return res.status(200).json({ ...others, accessToken })
-      }
+        const isValid = await bcrypt.compare(password, user.password);
+        if (isValid) {
+            const accessToken = jwt.sign({
+                id: user._id,
+                isAdmin: user._isAdmin || false
+            },
+                SECRET,
+                { expiresIn: "3d" }
+            );
+            const { password, ...others } = user._doc;
+            return res.status(200).json({ ...others, accessToken })
+        }
 
-   } catch (e) {
-      return next(e);
-   }
+    } catch (e) {
+        return next(e);
+    }
 })
 
 module.exports = router;
